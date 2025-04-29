9eb08401eedf9201b826368378ab8089e80380b4
MarcoUnie <mmagecoyt@gmail.com>
2025-04-29 10:59:43 +0000

    metodo para centrar la ventana de la gui

--- ui.py
+++ ui.py
@@ -1,9 +1,22 @@
 from tkinter import *
-class MainWindow(tk):
+
+class CenterWidgetMixin:
+    def center(self,):
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = int((ws/2) - (w/2))
+        y = int((hs/2) - (h/2))
+        self.geometry(f"{w}x{h}+{x}+{y}")
+
+class MainWindow(tk,CenterWidgetMixin):
     def __init__(self):
         super().__init__()
         self.title('Gestor de clientes')
         self.build()
+        self.center()
     def build(self):
         button = Button(self.root, text="Hola", command=self.hola)
         button.pack()
